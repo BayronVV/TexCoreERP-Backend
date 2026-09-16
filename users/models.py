9f7bf7efced3fe2279e2c66848aba193a960b416
BayronVV <bayronricardovr@ufps.edu.co>
2026-09-16 17:49:05 -0500

    feat: implement roles and admin dashboard

--- users/models.py
+++ users/models.py
@@ -3,10 +3,16 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     ROLE_CHOICES = (
+        ('PENDING', 'Pendiente'),
         ('ADMIN', 'Administrador'),
-        ('SECRETARY', 'Secretaria'),
+        ('VENDEDOR', 'Vendedor'),
+        ('GERENTE', 'Gerente'),
+        ('PRODUCCION', 'Jefe de Producción'),
+        ('ALMACENISTA', 'Almacenista'),
+        ('TERMINACION', 'Personal de Terminacion'),
+        ('SECRETARIA', 'Secretaria'),
     )
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='SECRETARY')
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='PENDING')
 
     def __str__(self):
         return f"{self.username} - {self.get_role_display()}"
